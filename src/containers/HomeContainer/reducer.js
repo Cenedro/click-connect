38ad75bc9f8e66c47822685fbce04ecfa83c9d85
Frederic B <frederic.bourguet@gmail.com>
2024-05-12 22:59:03 +0200

    Implement Decrement

--- src/containers/HomeContainer/reducer.js
+++ src/containers/HomeContainer/reducer.js
@@ -1,4 +1,4 @@
-import { INCREMENT } from './constant';
+import { DECREMENT, INCREMENT } from './constant';
 
 export const initialState = {
   counter: 0,
@@ -11,7 +11,12 @@ export default function homeContainerReducer(state = initialState, action) {
         ...state,
         counter: state.counter + 1,
       };
+    case DECREMENT:
+      return {
+        ...state, 
+        counter: state.counter > 0 ? state.counter - 1 : state.counter
+      };
     default:
-      return state;
+      return state
   }
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 function Home({
   onIncrement,
+  onDecrement,
   counter,
 }) {
   return (
     <div>
-      <button onClick={onIncrement}>
-        Vous avez cliqué {counter} fois
-      </button>
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onDecrement}>Decrement</button>
+      Vous avez cliqué {counter} fois
     </div>
   );
 };
